6ff075d068e4f5c77c48a317ffdd824afa30a69e
Matthew Rosell Felix <matthewgz241@gmail.com>
2020-01-31 20:07:48 -0500

    Se agrega los webpack.config para la construccion de produccion

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const AddAssetHtmlPlugin = require("add-asset-html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TersetJSPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, "src/index.js")
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[hash].js",
+    publicPath: "http://localhost:3001/",
+    chunkFilename: "js/[id].[chunkhash].js"
+  },
+  optimization: {
+    minimizer: [new TersetJSPlugin()]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            name: "[hash].[ext]",
+            outputPath: "assets"
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html")
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require("./modules-manifest.json")
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, "dist/js/*.dll.js"),
+      outputPath: "js",
+      publicPath: "http://localhost:3001/js"
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ["**/app.*", "**/modules.*"]
+    })
+  ]
+};
---
+++ webpack.dll.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const webpack = require("webpack");
+const TersetJSPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: {
+    modules: ["react", "react-dom"]
+  },
+  mode: "production",
+  optimization: {
+    minimizer: [new TersetJSPlugin()]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].[hash].dll.js",
+    library: "[name]"
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      name: "[name]",
+      path: path.join(__dirname, "[name]-manifest.json")
+    })
+  ]
+};
