10f9b07e9e09b8e7ff1c09f08434316ff8ee0f1d
matthewgz <matthewgz241@gmail.com>
2020-02-19 15:03:51 -0500

    Update index.js
    svg component

--- src/components/IconSismo/index.js
+++ src/components/IconSismo/index.js
@@ -1,15 +1,89 @@
 import React from "react";
-import { Div } from "./styles";
 
-export const IconSismo = () => {
-  return (
-    <Div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </Div>
-  );
-};
+const SVG = ({
+  style = {},
+  fill = "red",
+  width = "100%",
+  className = "",
+  viewBox = "0 0 100 100"
+}) => (
+  <svg
+    width={width}
+    style={style}
+    height={width}
+    viewBox={viewBox}
+    xmlns="http://www.w3.org/2000/svg"
+    className={`svg-icon ${className || ""}`}
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    preserveAspectRatio="xMidYMid meet"
+  >
+      <circle
+        cx="50"
+        cy="50"
+        r="25"
+        fill={fill}
+        fillOpacity="1"
+        stroke="rgba(166,3,17,1)"
+        strokeOpacity="1"
+        strokeWidth="2"
+        opacity="1"
+      ></circle>
+      <circle
+        cx="50"
+        cy="50"
+        r="20"
+        fill={fill}
+        fillOpacity="1"
+        stroke="rgba(166,3,17,1)"
+        strokeOpacity="1"
+        strokeWidth="2"
+        opacity="1"
+      ></circle>
+      <circle
+        cx="50"
+        cy="50"
+        r="15"
+        fill={fill}
+        fillOpacity="1"
+        stroke="rgba(166,3,17,1)"
+        strokeOpacity="1"
+        strokeWidth="2"
+        opacity="1"
+      ></circle>
+      <circle
+        cx="50"
+        cy="50"
+        r="10"
+        fill={fill}
+        fillOpacity="1"
+        stroke="rgba(166,3,17,1)"
+        strokeOpacity="1"
+        strokeWidth="2"
+        opacity="1"
+      ></circle>
+      <circle
+        cx="50"
+        cy="50"
+        r="5"
+        fill={fill}
+        fillOpacity="1"
+        stroke="rgba(166,3,17,1)"
+        strokeOpacity="1"
+        strokeWidth="2"
+        opacity="1"
+      ></circle>
+      <circle
+        cx="50"
+        cy="50"
+        r="0.5"
+        fill={fill}
+        fillOpacity="1"
+        stroke="rgba(166,3,17,1)"
+        strokeOpacity="1"
+        strokeWidth="2"
+        opacity="1"
+      ></circle>
+  </svg>
+);
+
+export default SVG;
